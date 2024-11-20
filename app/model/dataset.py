49125d5d8f02f7c108a65ec6965f8321554705f9
Umutcan Karakas <umutcankarakas@gmail.com>
2024-11-20 16:09:53 +0300

    Dataset analysis added

--- app/model/dataset.py
+++ app/model/dataset.py
@@ -4,10 +4,21 @@ from pydantic import BaseModel, HttpUrl
 from typing import List
 
 class DatasetInfo(BaseModel):
+    id: int
     name: str
     url: HttpUrl
     instances: int
     description: str
+    sensitive_features: dict
 
 class DatasetSelectionRequest(BaseModel):
     names: List[str]
+
+class DatasetAnalysis(BaseModel):
+    sensitive_column : str
+    model_accuracy : float
+    statistical_parity_difference : float
+    equal_opportunity_difference : float
+    average_odds_difference : float
+    disparate_impact : float
+    theil_index : float
---
+++ app/service/utils/dataset_utils.py
@@ -0,0 +1,134 @@
+from fairlearn.metrics import (
+    MetricFrame,
+    selection_rate,
+    true_positive_rate,
+    false_positive_rate
+)
+import pandas as pd
+pd.set_option('future.no_silent_downcasting', True)
+import numpy as np
+from scipy.stats import entropy
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LogisticRegression
+from sklearn.preprocessing import StandardScaler
+from sklearn.metrics import accuracy_score
+from ucimlrepo import fetch_ucirepo
+
+def calculate_statistical_parity_difference(X, y_true, y_pred, sensitive_column):
+    sr = lambda y_true, y_pred: selection_rate(y_true, y_pred)
+    mf = MetricFrame(metrics=sr, y_true=y_true, y_pred=y_pred, sensitive_features=X[sensitive_column])
+    return mf.difference(method='between_groups')
+
+def calculate_equal_opportunity_difference(X, y_true, y_pred, sensitive_column, pos_label=1):
+    tpr = lambda y_true, y_pred: true_positive_rate(y_true, y_pred, pos_label=pos_label)
+    mf = MetricFrame(metrics=tpr, y_true=y_true, y_pred=y_pred, sensitive_features=X[sensitive_column])
+    return mf.difference(method='between_groups')
+
+def calculate_average_odds_difference(X, y_true, y_pred, sensitive_column):
+    unique_labels = np.unique(y_true)
+    if len(unique_labels) == 2:
+        pos_label = unique_labels[1] 
+    else:
+        raise ValueError("y_true should have exactly two unique values for binary classification")
+
+    tpr = lambda y_true, y_pred: true_positive_rate(y_true, y_pred, pos_label=pos_label)
+    fpr = lambda y_true, y_pred: false_positive_rate(y_true, y_pred, pos_label=pos_label)
+    average_odds = lambda y_true, y_pred: (tpr(y_true, y_pred) + fpr(y_true, y_pred)) / 2
+
+    mf = MetricFrame(metrics=average_odds, 
+                     y_true=y_true, 
+                     y_pred=y_pred, 
+                     sensitive_features=X[sensitive_column])
+    return mf.difference(method='between_groups')
+
+def calculate_disparate_impact(X, y_true, y_pred, sensitive_column):
+    sr = lambda y_true, y_pred: selection_rate(y_true, y_pred)
+    mf = MetricFrame(metrics=sr, y_true=y_true, y_pred=y_pred, sensitive_features=X[sensitive_column])
+    return mf.ratio(method='between_groups')
+
+def calculate_theil_index(y_true, y_pred):
+    actual_pos = np.mean(y_true == 1)
+    pred_pos = np.mean(y_pred == 1)
+
+    epsilon = 1e-10
+
+    actual_entropy = -(actual_pos * np.log2(actual_pos + epsilon) + (1 - actual_pos) * np.log2(1 - actual_pos + epsilon))
+    pred_entropy = -(pred_pos * np.log2(pred_pos + epsilon) + (1 - pred_pos) * np.log2(1 - pred_pos + epsilon))
+
+    theil_index = pred_entropy - actual_entropy
+    return theil_index
+
+def initial_dataset_analysis(dataset_id):
+    if dataset_id == 1: dataset_name = "german"
+    elif dataset_id == 2: dataset_name = "adult"
+
+    dataset_dict = {"german" : 144, "adult" : 2}
+
+    dataset = fetch_ucirepo(id=dataset_dict[dataset_name]) 
+    
+    X = dataset.data.features 
+    y = dataset.data.targets 
+
+    if isinstance(y, pd.DataFrame):
+        y = y.squeeze()  # Convert DataFrame to Series if necessary
+
+    if dataset_name == "adult":
+        y = y.replace({'<=50K': 0, '<=50K.': 0, '>50K': 1, '>50K.': 1}).astype(int)
+        if 'age' in X.columns:
+            age_threshold = 50
+            X['age_binary'] = (X['age'] >= age_threshold).astype(int)
+            X.drop('age', axis=1, inplace=True)  # Remove the original 'age' column
+        if 'race' in X.columns:
+            X['race_binary'] = (X['race'] == 'White').astype(int)
+            X.drop('race', axis=1, inplace=True)  # Optionally remove the original 'race' column
+        sensitive_columns = ['sex_Female', 'age_binary', 'race_binary']  
+        sensitive_columns_display = {'sex_Female': 'Gender', 'age_binary': "Age", 'race_binary': "Race"}
+
+    elif dataset_name == "german":
+        if 'Attribute13' in X.columns:
+            age_threshold = 50
+            X['age_binary'] = (X['Attribute13'] >= age_threshold).astype(int)
+            X.drop('Attribute13', axis=1, inplace=True)  # Remove the original 'age' column
+        sensitive_columns = ['Attribute9_A91', 'age_binary'] 
+        sensitive_columns_display = {'Attribute9_A91': 'Gender',
+                                'age_binary' : 'Age'}
+
+    # Handling potential SettingWithCopyWarning correctly
+    X = X.copy().replace('?', np.nan).dropna()
+    y = y.loc[X.index]
+
+    # One-hot encode categorical variables
+    X = pd.get_dummies(X)
+    # Split data
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+
+    # Scaling features only at the point of training
+    scaler = StandardScaler()
+    X_train_scaled = scaler.fit_transform(X_train)
+    X_test_scaled = scaler.transform(X_test)
+
+    # Model training
+    model = LogisticRegression(random_state=42, max_iter=10000)
+    model.fit(X_train_scaled, y_train)
+
+    # Model prediction
+    y_pred = model.predict(X_test_scaled)
+
+    # Model evaluation
+    accuracy = accuracy_score(y_test, y_pred)
+
+    final_metrics = []
+    for sensitive_column in sensitive_columns:
+        if sensitive_column in X.columns:
+            final_metrics.append({
+                "Sensitive Column" : sensitive_columns_display[sensitive_column],
+                "Model Accuracy" : accuracy,
+                "Statistical Parity Difference" : calculate_statistical_parity_difference(X_test, y_test, y_pred, sensitive_column),
+                "Equal Opportunity Difference" : calculate_equal_opportunity_difference(X_test, y_test, y_pred, sensitive_column),
+                "Average Odds Difference" : calculate_average_odds_difference(X_test, y_test, y_pred, sensitive_column),
+                "Disparate Impact" : calculate_disparate_impact(X_test, y_test, y_pred, sensitive_column),
+                "Theil Index" : calculate_theil_index(y_test, y_pred)
+            })
+
+    return final_metrics
+
